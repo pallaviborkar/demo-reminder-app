3458c2d2d9d3ef820fd35adf6be9062109d0298f
pallaviborkar <borkar.pallavi@gmail.com>
2026-03-01 13:10:31 +0000

    add code for postConfirmation trigger lambda func

--- amplify/backend/function/reminderappuserpoolPostConfirmation/src/custom.js
+++ amplify/backend/function/reminderappuserpoolPostConfirmation/src/custom.js
@@ -3,5 +3,6 @@
  */
 exports.handler = async (event, context) => {
   // insert code to be executed by your lambda trigger
+  console.log("Post confirmation event:", JSON.stringify(event, null, 2));
   return event;
 };
